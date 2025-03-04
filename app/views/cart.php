4d1125ce0683a2957bdeedacc7eef7795c79524b
sinhpp <sinh.ern@student.passerellesnumeriques.org>
2025-03-04 19:49:43 +0700

    feat(user add): correct the project structure

--- app/views/cart.php
+++
@@ -1,5 +0,0 @@
-<!-- app/views/cart.php -->
-<?php foreach ($cartItems as $productId => $quantity): ?>
-    <p>Product ID: <?= $productId ?> - Quantity: <?= $quantity ?></p>
-<?php endforeach; ?>
-<a href="checkout">Proceed to Checkout</a>
--- app/views/checkout.php
+++
@@ -1,5 +0,0 @@
-<!-- app/views/checkout.php -->
-<form method="POST">
-    <input type="hidden" name="total_price" value="100.00">
-    <button type="submit">Place Order</button>
-</form>
